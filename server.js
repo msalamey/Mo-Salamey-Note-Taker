1d6fd29d4354325bec39bf31814f6b48e408b950
msalamey <m.salamey23@gmail.com>
2022-09-28 11:27:38 -0400

    made Heroku URL

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 // Sets up the Express App. 
 var app = express();
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 3001
 
 // Sets up the Express app to handle data parsing. 
 app.use(express.urlencoded({ extended: true }));
